1c8ad856adc6773b31e2f496b6b25a723398a2a1
nalgemi06 <dinar.khansa@finaccel.co>
2024-01-31 17:56:22 +0700

    hehe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.println("Hello guys");
         System.out.print("hehe");
+        System.out.print("hehe222");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
